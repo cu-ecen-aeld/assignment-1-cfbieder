8dcef7257e8bb519dca1f569ac2624e73a0e847b
cfbieder <cfbieder@Hotmail.com>
2022-08-28 18:01:40 +0000

    projet 1 done

---
+++ student-test/assignment1/Test_validate_username.c
@@ -0,0 +1,31 @@
+#include "unity.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "../../examples/autotest-validate/autotest-validate.h"
+#include "../../assignment-autotest/test/assignment1/username-from-conf-file.h"
+
+/**
+* This function should:
+*   1) Call the my_username() function in autotest-validate.c to get your hard coded username.
+*   2) Obtain the value returned from function malloc_username_from_conf_file() in username-from-conf-file.h within
+*       the assignment autotest submodule at assignment-autotest/test/assignment1/
+*   3) Use unity assertion TEST_ASSERT_EQUAL_STRING_MESSAGE to verify the two strings are equal.  See
+*       the [unity assertion reference](https://github.com/ThrowTheSwitch/Unity/blob/master/docs/UnityAssertionsReference.md)
+*/
+void test_validate_my_username()
+{
+    /**
+     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt
+     * config file and my_username() functions are setup properly
+     */
+
+    const char *name1 = my_username();
+    const char *name2 = malloc_username_from_conf_file() ;
+    //printf("Name 1 %s\n",name1 );
+    //printf("Name 2 %s\n",name2 );
+    bool check = !strcmp(name1,name2) ;
+    //printf("Check %d\n",check );
+    TEST_ASSERT_TRUE_MESSAGE(check,"AESD students, please fix me!");
+
+}
